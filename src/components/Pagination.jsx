2892c16cd51078718f90141a8db4be0de7984710
appletreeYann <107985273+appletreeYann@users.noreply.github.com>
2025-04-27 11:10:18 -0600

    Corregir errores en consola

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -6,7 +6,7 @@ export const Pagination = ({ currentPage, totalPages, onPageChange }) => {
           onClick={() => onPageChange(currentPage - 1)}
           className="px-2 py-1 bg-red-500 text-white rounded-md disabled:opacity-50 hover:cursor-pointer disabled:hover:cursor-auto"
         >
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" width="24" height="24" stroke-width="1">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" width="24" height="24" strokeWidth="1">
             <path d="M15 6l-6 6l6 6"></path>
           </svg>
         </button>
@@ -18,7 +18,7 @@ export const Pagination = ({ currentPage, totalPages, onPageChange }) => {
           onClick={() => onPageChange(currentPage + 1)}
           className="px-2 py-1 bg-red-500 text-white rounded-md disabled:opacity-50 hover:cursor-pointer disabled:hover:cursor-auto"
         >
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" width="24" height="24" stroke-width="1">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" width="24" height="24" strokeWidth="1">
             <path d="M9 6l6 6l-6 6"></path>
           </svg>
         </button>
--- src/services/api.js
+++ src/services/api.js
@@ -1,11 +1,9 @@
 const BASE_URL = "https://pokeapi.co/api/v2/";
-import { toast } from "react-toastify";
 
 export const getAllPokemons = () => {
   return fetch(`${BASE_URL}pokemon?limit=150&offset=0`)
     .then(response => {
       if (!response.ok) {
-        toast.error("Error al obtener los pokemon");
         throw new Error("Error al obtener los pokemon");
         
       }
